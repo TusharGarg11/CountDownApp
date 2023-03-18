ecd743b5b06cc462af45b1554e964175b9b3f524
TusharGarg11 <tushar.garg.learningAc@gmail.com>
2023-03-18 22:41:46 +0530

    Add the Alert Box for Wishing BirthDay

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-const givenBirthdayDate="21 March 2023 10:00 PM";
+const givenBirthdayDate="18 March 2023 10:41 PM";
 const inputs=document.querySelectorAll("input");
+
 
 function clockCounter(){
     const givenDate=new Date(givenBirthdayDate);
@@ -9,10 +10,13 @@ function clockCounter(){
     inputs[1].value=Math.floor((diff/3600)%24);
     inputs[2].value=Math.floor((diff/60)%60);
     inputs[3].value=Math.floor(diff%60);
-
+    if(inputs[0].value==0 && inputs[1].value==0 && inputs[2].value==0 && inputs[3].value==0){
+        document.getElementById("alertDisplayBox").style.display="block";
+        clearInterval(CounterInterval);
+    }
 }
 
-setInterval(() => {
+const CounterInterval=setInterval(() => {
     clockCounter();
 }, 1000);
 
